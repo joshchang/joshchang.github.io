140c15ac0a22ea2673eebc2a25b365668832022a
Josh Chang <joshchang@ucla.edu>
2019-10-10 23:56:39 -0400

    spelling

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "The biggest hurdle we had to overcome in formulating a calcium hypothesis for post-CSD vasoconstriction",
+        "excerpt":"About a decade ago now, I entered the field of biology by working in a neuroscience wet lab, studying this phenomenon known as cortical spreading depolarization (depression), or CSD. CSD is a physical wave of ionic movement and activity in cortical brain tissue that occurs in all sorts of pathological...","categories": ["Blog","Research"],
+        "tags": ["research","CSD","calcium","ODE"],
+        "url": "http://localhost:4000/blog/research/postacute-csd/",
+        "teaser":null}]
